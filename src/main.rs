d06f1d5a6ed9e005e9ed18d4cfcbbf1f4157c6d4
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2023-03-19 19:44:46 +0900

    :art: tokioに変更

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,81 @@
-use std::{net::{TcpListener, TcpStream, SocketAddr}, thread, io::{Read, Write}, sync::{Arc, Mutex}};
-use crossbeam_channel::{unbounded, Sender};
+use std::{net::{SocketAddr}, sync::Arc};
+use tokio::{io::{AsyncReadExt}, net::{TcpListener, TcpStream}, sync::{broadcast::{self, Sender}, Mutex}};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind.");
-    let (message_tx, message_rx) = unbounded::<String>();
-    let (close_tx, close_rx) = unbounded::<SocketAddr>();
-    let clients: Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(Vec::new()));
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let (message_tx, mut message_rx) = broadcast::channel::<String>(10);
+    let (close_tx, mut close_rx) = broadcast::channel::<SocketAddr>(10);
+    let mut clients: Vec<Arc<Mutex<TcpStream>>> = vec![];
 
-    let clients_clone = Arc::clone(&clients);
-    // clientのクローズを待機
-    thread::spawn(move || {
-        for addr in close_rx {
-            let index = clients_clone.lock().unwrap().iter().position(|x| x.peer_addr().unwrap() == addr).unwrap();
-            clients_clone.lock().unwrap().remove(index);
-        }
-    });
-    
-    let clinets_clone = Arc::clone(&clients);
-    // メッセージの配信
-    thread::spawn(move || {
-        loop {
-            let message = message_rx.recv().unwrap();
-            for mut client in clinets_clone.lock().unwrap().iter() {
-                client.write_all(message.as_bytes()).unwrap();
+    // 接続待機
+    loop {
+        tokio::select! {
+            // 接続要求時
+            result = listener.accept() => {
+                match result {
+                    Ok((stream, addr)) => {
+                        let shared_stream = Arc::new(Mutex::new(stream));
+                        clients.push(shared_stream.clone());
+                        let message_tx_clone = message_tx.clone();
+                        let close_tx_clone = close_tx.clone();
+                        let socket_addr = addr.clone();
+                        tokio::spawn(async move {
+                            handler(shared_stream.clone(), socket_addr, message_tx_clone, close_tx_clone).await;
+                        });
+                    },
+                    Err(e) => {
+                        println!("Error: {}", e);
+                    }
+                }
             }
-        }
-    });
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                clients.lock().unwrap().push(stream.try_clone().unwrap());
-                let message_tx_clone = message_tx.clone();
-                let close_tx_clone = close_tx.clone();
-                thread::spawn(move || {
-                    handler(stream, message_tx_clone.clone(), close_tx_clone.clone());
-                });
-            },
-            Err(e) => {
-                println!("Error: {}", e);
+            // クライアント切断時
+            Ok(addr) = close_rx.recv() => {
+                let index =
+                    clients.iter().position(|x| x.blocking_lock().peer_addr().unwrap() == addr).unwrap();
+                clients.remove(index);
+            }
+            // メッセージ受信時
+            Ok(message) = message_rx.recv() => {
+                for client in clients.iter_mut() {
+                    if let Err(_) = client.lock().await.try_write(message.as_bytes()) {
+                        continue;
+                    }
+                }
             }
         }
     }
 }
 
-fn handler(mut stream: TcpStream, message_tx: Sender<String>, close_tx: Sender<SocketAddr>) {
+async fn handler(stream: Arc<Mutex<TcpStream>>, addr: SocketAddr, message_tx: broadcast::Sender<String>, close_tx: broadcast::Sender<SocketAddr>) {
     let mut buffer = vec![];
 
-    let socket_addr = stream.peer_addr().unwrap().clone();
+    let socket_addr = addr.clone();
+
+    let message_tx_clone = message_tx.clone();
     let close = move || {
+        message_tx_clone.send(format!("{} is disconnected.\n", addr)).unwrap();
         close_tx.send(socket_addr).unwrap();
     };
 
     loop {
-        match stream.read(&mut buffer) {
-            Ok(0) => {
+        match tokio::time::timeout(std::time::Duration::from_secs(600), stream.lock().await.read(buffer.as_mut_slice())).await {
+            Ok(Err(_)) | Err(_) => {
                 close();
                 break;
             },
-            Ok(n) => {
+            Ok(Ok(0)) => {
+                close();
+                break;
+            },
+            Ok(Ok(n)) => {
+                let message_tx_clone = Sender::clone(&message_tx);
+                let socket_addr = addr.clone();
                 let message = String::from_utf8_lossy(&buffer[..n]);
-                message_tx.send(message.into_owned()).unwrap();
-            },
-            Err(e) => {
-                println!("Error: {}", e);
-                close();
-                break;
+                message_tx_clone.send(format!("{} : {}", socket_addr, message)).unwrap();
             }
         }
+
+        buffer.clear();
     }
 }
